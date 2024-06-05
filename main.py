9922f5645a73ff6f311841eb080a67c2e3cba8b3
ZhenyaRom <evladro2020@gmail.com>
2024-06-05 18:55:40 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print ("Hello> world!")
+a = -1
+print(type(a), a)
+b = str(a)
+print(type(b), b)
+c = bool(a)
+d = bool(b)
+print(type(c), c)
+print(type(d), d)
+e = 'Hello, world!'
+print(e[-114])
